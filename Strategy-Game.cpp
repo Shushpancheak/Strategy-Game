ae7d9a50631f62102a6d6da52740e1db1c516e26
Maximelian Mumladze <Makcieml@gmail.com>
2019-03-27 15:02:51 +0300

    Setted VS CMake project. Yay

---
+++ Strategy-Game.cpp
@@ -0,0 +1,11 @@
+﻿// Strategy-Game.cpp: определяет точку входа для приложения.
+//
+
+#include "Strategy-Game.h"
+
+using namespace std;
+
+int main() {
+	cout << "Hello CMake." << endl;
+	return 0;
+}
